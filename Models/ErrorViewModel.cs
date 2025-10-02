8ef8d0424d532b15daaf502110662cbcd0bd3940
IQBALNAFIS1310 <iqbalnafis487@gmail.com>
2025-10-02 14:02:32 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PenjualanOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
